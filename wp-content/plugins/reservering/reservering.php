c447cb4d045fa3669711664442dfec3af1617c3f
AutiCodes <prive@auticodes.nl>
2024-11-29 12:26:29 +0100

    Added comment

--- wp-content/plugins/reservering/reservering.php
+++ wp-content/plugins/reservering/reservering.php
@@ -208,6 +208,9 @@ function reservation_customers_page() {
     <?php
 }
 
+/**
+ * The houses page
+ */
 function reservation_houses_page() {
     ?>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
