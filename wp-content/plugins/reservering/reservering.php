b9183047006765e5e8aca01edb19e95645046a8f
AutiCodes <prive@auticodes.nl>
2024-11-29 12:20:58 +0100

    Updated reservation table

--- wp-content/plugins/reservering/reservering.php
+++ wp-content/plugins/reservering/reservering.php
@@ -26,6 +26,15 @@ function create_menus() {
         'reserveringen-customers',
         'reservation_customers_page',
     );
+
+    add_submenu_page(
+        'reserveringen',
+        'Huizen',
+        'Huizen',
+        'manage_options',
+        'reserveringen-huizen',
+        'reservation_houses_page',
+    );
 }
 
 add_action('admin_menu', 'create_menus');
@@ -49,10 +58,9 @@ function reservation_options_page() {
                 <th scope="col">Naam</th>
                 <th scope="col">Begin datum</th>
                 <th scope="col">Eind datum</th>
-                <th scope="col">Aantal nachten</th>
-                <th scope="col">Beschikbaarheid</th>
-                <th scope="col">Betaling status</th>
                 <th scope="col">Aangemaakt op</th>
+                <th scope="col">Aantal nachten</th>
+                <th scope="col">Betaling status</th>
                 <th scope="col">Status</th>
                 <th scope="col">Weergeven</th>
             </tr>
@@ -60,16 +68,15 @@ function reservation_options_page() {
         <tbody>
             <tr>
                 <td scope="row">2</td>
-                <td>Voorbeeld huis</td>
-                <td>Kelvin de Reus</td>
+                <td><a href="#">Voorbeeld huis</a></td>
+                <td><a href="#">Kelvin de Reus</a></td>
                 <td>29-11-2024</td>
                 <td>30-11-2024</td>
+                <td>29-11-2024</td>
                 <td>1</td>
-                <td>Beschikbaar</td>
                 <td>
                     <span class="badge text-bg-danger">NIET BETAALD</span>
                 </td>
-                <td>29-11-2024</td>
                 <td>
                     <span class="badge text-bg-danger">IN BEHANDELING</span>
                 </td>
@@ -81,16 +88,15 @@ function reservation_options_page() {
             </tr>
             <tr>
                 <td scope="row">1</td>
-                <td>Voorbeeld huis</td>
-                <td>Kelvin de Reus</td>
+                <td><a href="#">Voorbeeld huis</a></td>
+                <td><a href="#">Kelvin de Reus</a></td>
                 <td>29-11-2024</td>
                 <td>30-11-2024</td>
+                <td>29-11-2024</td>
                 <td>1</td>
-                <td>Beschikbaar</td>
                 <td>
                     <span class="badge text-bg-success">BETAALD</span>
                 </td>
-                <td>29-11-2024</td>
                 <td>
                     <span class="badge text-bg-success">BEHANDELD</span>
                 </td>
@@ -197,6 +203,18 @@ function reservation_customers_page() {
             </tr>
         </tbody>
     </table>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <?php
+}
+
+function reservation_houses_page() {
+    ?>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <h1 class="mt-2 ml-2">Huizen</h1>
+
+    <hr>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <?php
