cd329766de5d0d604be6d1aea3ac3e1c232104dd
AutiCodes <prive@auticodes.nl>
2024-11-29 13:29:17 +0100

    Added financial page

--- wp-content/plugins/reservering/reservering.php
+++ wp-content/plugins/reservering/reservering.php
@@ -46,6 +46,15 @@ function create_menus() {
 
     add_submenu_page(
         'reserveringen',
+        'Omzet',
+        'Omzet',
+        'manage_options',
+        'reserveringen-omzet',
+        'reservation_financial_page',
+    );
+
+    add_submenu_page(
+        'reserveringen',
         'Instellingen',
         'Instellingen',
         'manage_options',
@@ -328,6 +337,21 @@ function reservation_houses_page() {
             </tr>
         </tbody>
     </table>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <?php
+}
+
+/**
+ * The finance page
+ */
+function reservation_financial_page() {
+    ?>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <h1 class="mt-2 ml-2">Omzet</h1>
+
+    <hr>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <?php
