95c28ccbe86a28eb1d70f7e798c3bcfbd8e4c087
AutiCodes <prive@auticodes.nl>
2024-12-01 21:29:00 +0100

    Added basic new customer page

--- wp-content/plugins/reservering/reservering.php
+++ wp-content/plugins/reservering/reservering.php
@@ -37,6 +37,15 @@ function create_menus() {
     );
 
     add_submenu_page(
+        '',
+        '',
+        '',
+        'manage_options',
+        'reserveringen-customers-aanmaken',
+        'reservation_create_customers_page',
+    );
+
+    add_submenu_page(
         'reserveringen',
         'Huizen',
         'Huizen',
@@ -354,6 +363,12 @@ function reservation_customers_page() {
 
     <hr>
 
+    <div class="mt-2 mb-2">
+        <a href="/wp-admin/index.php?page=reserveringen-customers-aanmaken">
+            <button type="button" class="btn btn-success">Nieuw</button>
+        </a>
+    </div>
+
     <div class="container">
         <table class="table table-striped">
             <thead>
@@ -434,6 +449,25 @@ function reservation_customers_page() {
                 </tr>
             </tbody>
         </table>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <?php
+}
+
+/**
+ * Create customers page
+ */
+function reservation_create_customers_page() {
+    ?>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <h1 class="mt-2 ml-2">Nieuwe klant</h1>
+
+    <hr>
+
+    <div class="container">
+
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
